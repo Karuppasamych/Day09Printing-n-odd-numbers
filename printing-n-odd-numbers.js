67c2b89eb64c30449ac63dc7e89f9ce1c5b4f020
Karuppasamych <106671462+Karuppasamych@users.noreply.github.com>
2022-06-22 12:58:44 +0530

    Add files via upload

---
+++ printing-n-odd-numbers.js
@@ -0,0 +1,11 @@
+let l = prompt('Enter the number')
+function printOdd(num){
+    
+        for(let i=0; i <=num; i++){
+            if(i %  2 == 1){
+                console.log(`Printing n odd numbers, ${i}`)
+            }
+        }
+}
+
+var printOddnumber = printOdd(l)
